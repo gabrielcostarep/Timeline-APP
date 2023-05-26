d6948ce9de3850c80a7caab55aea79ccd846b5c2
Gabriel Costa <gabrielcostadev@hotmail.com>
2023-05-26 18:54:29 -0300

    mobile +/- finish

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -5,17 +5,81 @@ import {
   Text,
   TextInput,
   ScrollView,
+  Image,
 } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { Link } from 'expo-router'
-import Icon from '@expo/vector-icons/Feather'
 import { useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Link, useRouter } from 'expo-router'
+import Icon from '@expo/vector-icons/Feather'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
 
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 export default function NewMemory() {
+  const router = useRouter()
+
   const { bottom, top } = useSafeAreaInsets()
+
   const [isPublic, setIsPublic] = useState(false)
+  const [preview, setPreview] = useState<string | null>(null)
+  const [content, setContent] = useState('')
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (error) {
+      // Não tratei o erro
+    }
+  }
+
+  async function handleCreateMemoy() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      coverUrl = uploadResponse.data.fileUrl
+    }
+
+    await api.post(
+      '/memories',
+      {
+        content,
+        coverUrl,
+        isPublic,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/memories')
+  }
 
   return (
     <ScrollView
@@ -47,18 +111,30 @@ export default function NewMemory() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={openImagePicker}
           className="h-20 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#FFF" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              alt="Imagem escolhida pelo usuário"
+              className="h-full w-full rounded-lg object-cover"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#FFF" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
+          textAlignVertical="top"
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor={'#56565a'}
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
@@ -67,6 +143,7 @@ export default function NewMemory() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="self-end rounded-full bg-green-500 px-5 py-2"
+          onPress={handleCreateMemoy}
         >
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
         </TouchableOpacity>
